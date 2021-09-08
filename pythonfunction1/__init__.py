98aab514be8a2c551bac3e58d789229be557cf0b
sumeagar1 <sumeeta@cloudthat.com>
2021-09-08 15:12:57 +0530

    added new line 21

--- pythonfunction1/__init__.py
+++ pythonfunction1/__init__.py
@@ -18,6 +18,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     if name:
         return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
         print ("hello world1")
+        print ("hello")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
